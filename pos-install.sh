29e161a62828fc5ecb2c80ee2d3725c3631c170a
Leonardo / LuKi <leo.monteiro06@live.com>
2021-08-22 12:56:52 -0300

    Fix comments; add xdg-user-dirs-update; fix folder

--- pos-install.sh
+++ pos-install.sh
@@ -2,32 +2,36 @@
 # Shell script to configure some things
 set -e
 
+# Create user folders
+xdg-user-dirs-update
+
 # Download polybar-themes and rofi (credits for adi1090x)
+# Install manually
 git clone --depth=1 https://github.com/adi1090x/polybar-themes.git $HOME/polybar-themes
 chmod +x $HOME/polybar-themes/setup.sh
 git clone --depth=1 https://github.com/adi1090x/rofi $HOME/rofi
 chmod +x $HOME/rofi/setup.sh 
 
-# Configure i3
+# Setup i3
 cp Configs/i3config $HOME/.config/i3/config
 
-# Configure kitty
+# Setup kitty
 mkdir -p $HOME/.config/kitty
 cp Configs/kitty.conf $HOME/.config/kitty
 
-# Configure picom
+# Setup picom
 mkdir -p $HOME/.config/picom
 cp Configs/picom.conf $HOME/.config/picom
 
-# Configure dunst
+# Setup dunst
 mkdir -p $HOME/.config/dunst
 cp Configs/dunstrc $HOME/.config/dunst
 
-# Transfer wallpaper
-mkdir -p $HOME/Imagens/Wallpapers
-cp Wallpaper/1.jpg $HOME/Imagens/Wallpapers
+# Move wallpaper to ~/Pictures/Wallpapers
+mkdir -p $HOME/Pictures/Wallpapers
+cp Wallpaper/1.jpg $HOME/Pictures/Wallpapers
 
-# Transfer wallpaper to /usr/share/backgrounds (because of LightDM)
+# Move wallpaper to /usr/share/backgrounds (because of LightDM)
 sudo mkdir -p /usr/share/backgrounds
 sudo cp Wallpaper/1.jpg /usr/share/backgrounds
 
