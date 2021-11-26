ecc8f033081c084c2718a97a568285d07f5da31c
Leonardo / LuKi <leo.monteiro06@live.com>
2021-11-26 07:51:11 -0300

    Disable bluetooth support

--- Nix/configuration.nix
+++ Nix/configuration.nix
@@ -168,7 +168,7 @@
   hardware.sane.extraBackends = [ pkgs.utsushi ];
 
   # Enable Bluetooth support.
-  hardware.bluetooth.enable = true;
+  hardware.bluetooth.enable = false;
   services.blueman.enable = false;
 
   # Enable sound and PulseAudio.
